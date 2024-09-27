7b412b530417ddd8e87d3309600ecc39997973d8
jgregorio <gregoriojesus0@gmail.com>
2024-09-27 18:08:43 +0200

    Modified test for parse only

--- src/test/java/dev/jgregorio/handle/ticket/ParseTicketAdapterTest.java
+++ src/test/java/dev/jgregorio/handle/ticket/ParseTicketAdapterTest.java
@@ -4,6 +4,7 @@ import dev.jgregorio.handle.ticket.domain.model.LoadedTicket;
 import dev.jgregorio.handle.ticket.domain.model.ParsedTicket;
 import dev.jgregorio.handle.ticket.domain.model.Ticket;
 import dev.jgregorio.handle.ticket.infrastructure.api.GoogleVisionApi;
+import dev.jgregorio.handle.ticket.infrastructure.api.ParseTicketAdapter;
 import dev.jgregorio.handle.ticket.infrastructure.filesystem.FileSystemLoader;
 import dev.jgregorio.handle.ticket.infrastructure.filesystem.LoadTicketAdapter;
 import org.junit.jupiter.api.Test;
@@ -16,23 +17,21 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 //@SpringBootTest
 class ParseTicketAdapterTest {
 
+    private LoadTicketAdapter loadTicketAdapter = new LoadTicketAdapter(
+            new FileSystemLoader());
+    ParseTicketAdapter parseTicketAdapter = new ParseTicketAdapter(
+            new GoogleVisionApi());
+
     @Test
     void givenImage_whenAnnotateImage_thenReturn() throws IOException {
         // GIVEN ticket with image in classpath
         Ticket ticket = Ticket.builder().
                 imagePath("/ticket-mercadona.jpg").build();
-        // WHEN load data
-        LoadedTicket loadedData = new LoadTicketAdapter(
-                new FileSystemLoader())
-                .loadTicket(ticket);
-        // THEN data loaded is not null
-        assertNotNull(loadedData.getData());
         // WHEN parse data
-        ParsedTicket parsedData = new dev.jgregorio.handle.ticket.infrastructure.api.ParseTicketAdapter(
-                new GoogleVisionApi())
-                .parseTicket(loadedData);
+        LoadedTicket loadedTicket = loadTicketAdapter.loadTicket(ticket);
+        ParsedTicket parsedTicket = parseTicketAdapter.parseTicket(loadedTicket);
         // THEN data parsed is not null
-        assertNotNull(parsedData.getData());
+        assertNotNull(parsedTicket.getData());
     }
 
 }
