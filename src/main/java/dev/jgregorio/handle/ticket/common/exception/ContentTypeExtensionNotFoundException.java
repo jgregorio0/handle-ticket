0e62ff78b9e607da66454974b548ba1c8dd82099
jgregorio <gregoriojesus0@gmail.com>
2024-10-05 23:45:15 +0200

    Added extension not found exception when cannot parse extension from content type

---
+++ src/main/java/dev/jgregorio/handle/ticket/common/exception/ContentTypeExtensionNotFoundException.java
@@ -0,0 +1,8 @@
+package dev.jgregorio.handle.ticket.common.exception;
+
+public class ContentTypeExtensionNotFoundException extends Exception {
+
+    public ContentTypeExtensionNotFoundException(String message) {
+        super(message);
+    }
+}
