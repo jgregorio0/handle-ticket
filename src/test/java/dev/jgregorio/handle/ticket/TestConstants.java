fcf9ae1babe1ff25b8e11c95f379e1c7a4520f01
jgregorio <gregoriojesus0@gmail.com>
2024-10-14 23:00:52 +0200

    Update non existing ticket path

--- src/test/java/dev/jgregorio/handle/ticket/TestConstants.java
+++ src/test/java/dev/jgregorio/handle/ticket/TestConstants.java
@@ -1,7 +1,11 @@
 package dev.jgregorio.handle.ticket;
 
+import java.net.URL;
+import java.util.Objects;
+
 public class TestConstants {
-    public static String TICKET_PATH = "/ticket-mercadona.jpg";
-    public static String NON_EXISTING_TICKET_PATH = "/ticket-mercadona.jpg";
-    public static String TICKET_JSON_PATH = "/ticket-mercadona-text-annotations.json";
+    public static final String TICKET_PATH = "/ticket-mercadona.jpg";
+    public static final String NON_EXISTING_TICKET_PATH = "/non-existing-ticket.jpg";
+    public static final String TICKET_JSON_PATH = "/ticket-mercadona-text-annotations.json";
+    public static final URL TICKET_URL = Objects.requireNonNull(TestConstants.class.getResource(TICKET_PATH));
 }
