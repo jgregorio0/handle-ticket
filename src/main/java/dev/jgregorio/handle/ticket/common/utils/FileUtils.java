a48e0960079169ac3715ce22bf851690bf85d89e
jgregorio <gregoriojesus0@gmail.com>
2024-10-14 22:59:02 +0200

    Update File Utils to return bytes array

--- src/main/java/dev/jgregorio/handle/ticket/common/utils/FileUtils.java
+++ src/main/java/dev/jgregorio/handle/ticket/common/utils/FileUtils.java
@@ -2,35 +2,42 @@ package dev.jgregorio.handle.ticket.common.utils;
 
 import dev.jgregorio.handle.ticket.common.exception.FileSystemResourceReadException;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
 
 public class FileUtils {
 
-    public static String readLargeFileStringByUrl(URL fileUrl) throws IOException {
-        StringBuilder stringBuilder = new StringBuilder();
-        try (BufferedReader br = new BufferedReader(
-                new FileReader(fileUrl.getPath()))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                stringBuilder.append(line);
+    public static byte[] readFileAsBytesBuffered(URL fileUrl) {
+        try (InputStream inputStream = fileUrl.openStream()) {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+            byte[] buffer = new byte[8192];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
             }
+
+            return outputStream.toByteArray();
+        } catch (IOException e) {
+            // Handle exception appropriately
+            throw new FileSystemResourceReadException("Resource could not be read: " + fileUrl);
         }
-        return stringBuilder.toString();
     }
+
 
     public static byte[] readFileBytesByUrl(URL fileUrl) throws FileSystemResourceReadException {
         return Optional.ofNullable(fileUrl)
                 .map(url -> {
                     try {
                         // read file bytes
-                        return Files.readAllBytes(Path.of(url.getPath()));
-                    } catch (IOException e) {
+                        return Files.readAllBytes(Paths.get(url.toURI()));
+                    } catch (IOException | URISyntaxException e) {
                         // TODO log error
                         throw new FileSystemResourceReadException("Resource could not be read: " + fileUrl);
                     }
